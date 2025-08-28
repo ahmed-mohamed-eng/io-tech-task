714a4f0fa991cf1b328c4dd1283b652f37f5c8d5
Ahmed Mohamed Karmy <ahmed.mohamed.eng05@gmail.com>
2025-08-28 15:27:27 +0300

    updated the background color

---
+++ components/OurTeams/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OurTeams = () => {
+  return (
+    <section className='flex flex-col items-center space-y-8'>
+        
+    </section>
+  )
+}
+
+export default OurTeams;
